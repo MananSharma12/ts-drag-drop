0eedf6c72329f4d9f65e9aaa6ccb669881758ce2
MananSharma12 <manan.sharma1205@gmail.com>
2023-08-24 03:12:37 +0530

    Added droppable area

--- src/app.ts
+++ src/app.ts
@@ -159,7 +159,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
     @autobind
     dragStartHandler(event: DragEvent) {
-        console.log(event);
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent) {
@@ -190,12 +191,16 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
 
     @autobind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
     }
 
-    dropHandler(_: DragEvent) {
+    dropHandler(event: DragEvent) {
+        console.log(event);
     }
 
     @autobind
