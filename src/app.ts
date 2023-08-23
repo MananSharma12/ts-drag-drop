73ae4121fcb3760b65b7dc3642eda411e2fb0af3
MananSharma12 <manan.sharma1205@gmail.com>
2023-08-24 03:20:26 +0530

    Finished drag & drop

--- src/app.ts
+++ src/app.ts
@@ -63,6 +63,18 @@ class ProjectState extends State<Project> {
             ProjectStatus.Active
         );
         this.projects.push(newProject)
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId)
+        if(project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for(const listenerFn  of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -199,8 +211,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autobind
     dropHandler(event: DragEvent) {
-        console.log(event);
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autobind
