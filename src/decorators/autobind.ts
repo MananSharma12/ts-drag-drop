aec76977f01d83abe6ca1ac9c0086cbac7277acd
MananSharma12 <manan.sharma1205@gmail.com>
2023-08-24 23:35:33 +0530

    Cleaned up file with namespaces

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+    // autobind decorator
+    export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                return originalMethod.bind(this);
+            }
+        };
+        return adjDescriptor;
+    }
+}
